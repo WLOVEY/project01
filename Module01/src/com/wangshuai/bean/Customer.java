77cd94fc5d23ee7e55c108a30a4b7c943083acfa
WangBinShuai <799956748@qq.com>
2018-07-21 14:51:51 +0800

    Initial commit

---
+++ Module01/src/com/wangshuai/bean/Customer.java
@@ -0,0 +1,13 @@
+package com.wangshuai.bean;
+//注释
+public class Customer {
+
+    public static void main(String[] args){
+        System.out.println("Hello!!!");
+
+    }
+
+    public void cry(){
+
+    }
+}
---
+++ src/com/wangshuai/java/Demo1.java
@@ -0,0 +1,12 @@
+package com.wangshuai.java;
+
+/**
+ * @author 王帅
+ * @create 2018-07-20 20:02
+ */
+
+public class Demo1 {
+    public static void main(String[] args) {
+
+    }
+}
